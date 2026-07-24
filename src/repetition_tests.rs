be8551c379eea8fdc0029460eabe828b35ce4c3e
Paul Saunders <coruscateor@users.noreply.github.com>
2026-07-24 16:23:14 +1200

    - Removed the paste dev-dependency.
    - Added the pastey dev-dependency and updated the project accordingly.
    
    - Added a rule to the impl_val_setter macro which takes an ident parameter and a ty parameter in curly braces, requiring one or more repetitions in this format, outputting setter method definitions.
    
    - Added a rule to the impl_val_setter macro which takes an ident parameter, a ty parameter and a literal parameter in curly braces, requiring one or more repetitions in this format, outputting documented setter method definitions.
    
    - Added a rule to the impl_trait_val_setter macro which takes an ident parameter and a ty parameter in curly braces, requiring one or more repetitions in this format, outputting setter method definitions.
    
    - Added a rule to the impl_trait_val_setter macro which takes an ident parameter, a ty parameter and a literal parameter in curly braces, requiring one or more repetitions in this format, outputting documented setter method definitions.
    
    - Removed the tests lib-module.
    
    - Added the tests test module.
    
    - Moved the contents of the tests lib-module into the newly added tests module.
    
    - Added the repetition_tests test module.

---
+++ src/repetition_tests.rs
@@ -0,0 +1,67 @@
+use super::*;
+
+use pastey::paste;
+
+#[derive(Default)]
+struct TestStruct
+{
+
+    a_number: i32,
+    a_bool: bool,
+    a_string: String,
+    a_number_doc: i8,
+    a_string_doc: String,
+    some_numbers: Vec<i32>,
+    some_bools: Vec<bool>,
+
+}
+
+impl TestStruct
+{
+
+    pub fn new() -> Self
+    {
+
+        Self::default()
+
+    }
+
+    impl_ref_getter!(a_number, i32);
+
+    impl_val_setter!({a_number, i32}, {some_numbers, Vec<i32>});
+
+    impl_val_setter!({a_bool, bool, "Sets a bool"}, {some_bools, Vec<bool>, "Sets some bools"});
+
+    impl_ref_getter!(a_string, String);
+
+    impl_ref_getter!(a_string_doc, String, "Returns a cloned String.");
+
+    impl_ref_getter!(some_numbers, Vec<i32>, "Returns some numbers by reference.");
+
+    impl_mut_getter!(some_numbers, Vec<i32>, "Returns some numbers by mutable reference.");
+
+    impl_val_getter!(some_numbers, Vec<i32>);
+
+    //impl_val_setter!(some_numbers, Vec<i32>);
+
+}
+
+#[test]
+fn it_works()
+{
+
+    //use super::tests::TestStruct;
+    
+    let mut test_struct = TestStruct::new();
+
+    test_struct.set_a_number(12);
+
+    test_struct.set_some_numbers(vec![3, 4, 78906]);
+
+    //let _number = test_struct.a_number();
+
+    test_struct.set_a_number(5);
+
+    //test_struct.
+
+}
